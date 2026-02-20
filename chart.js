8f52183944e153d2744ed76ea6081a12bae07e32
Jumana jadheeda pj <149096011+jumanajadheeda@users.noreply.github.com>
2026-02-21 01:48:34 +0530

    Updated roadmap system and added visual roadmap pages

---
+++ chart.js
@@ -0,0 +1,27 @@
+<>
+  <div class="roadmap-container">
+      <div class="roadmap-line"></div>
+
+      <div class="roadmap-stage" data-level="Junior Associate">
+          Junior Associate
+      </div>
+
+      <div class="roadmap-stage" data-level="Associate Lawyer">
+          Associate Lawyer
+      </div>
+
+      <div class="roadmap-stage" data-level="Senior Lawyer">
+          Senior Lawyer
+      </div>
+
+      <div class="roadmap-stage" data-level="Partner">
+          Partner / Legal Head
+      </div>
+  </div>
+
+  <div id="stageDetails" class="stage-details"></div>
+
+  <button id="downloadRoadmap" class="download-btn">
+      Download Roadmap
+  </button>
+</>
